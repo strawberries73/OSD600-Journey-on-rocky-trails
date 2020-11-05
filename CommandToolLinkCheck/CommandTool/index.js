820e0d1060f7113c00df9cbf1ec09477dc89ef5d
strawberries73 <39388566+strawberries73@users.noreply.github.com>
2020-11-05 12:14:45 -0500

    comit

--- CommandToolLinkCheck/CommandTool/index.js
+++ CommandToolLinkCheck/CommandTool/index.js
@@ -2,15 +2,14 @@ const fs=require('fs')
 const fetch=require('node-fetch')
 const path=require('path')
 const packageJson = require('./package.json');
+const colors = require('colors');
 
-console.log(process.argv)
 
-
-//Flag labeling each URL as a good or bad
-const _label = ({
-    good: "GOOD",
-    bad: "BAD"
-})
+const greetingMessage=()=>{
+console.log("This is command line tool");
+console.log("if red links are not working");
+console.log("if green links are working")
+}
 
 // ExitCode
 process.on('SIGTERM', () => {
@@ -19,38 +18,38 @@ process.on('SIGTERM', () => {
     })
   })
 
-if(process.argv.length == 2){
-    greetingMessage();
-    console.log("Hello");
-}
-else{ // more then 2
+//Flag labeling each URL as a good or bad
+const _label = ({
+    good: "GOOD",
+    bad: "BAD"
+})
 
-    if (process.argv[2] == "v" || process.argv[2] == "version") {
-        console.log(packageJson.name + " Version " + packageJson.version);
-    } else{
-        const filePath=path.join(__dirname,process.argv[2])
-        fs.readFile(filePath,'utf-8',(err,data)=>{
-            if(err){
-                console.log("Fail to read file",err)
-            }
-            else{
-                const validUrl=data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi)
-                validUrl.forEach((url)=>{
-                fetch(url,{method:'HEAD',timeout:2000})
-                .then((res)=>{
-                    if(res.status==200)
-                    console.log(res.status,url.green,_label.good.rainbow)
-                    else if(res.status==400||res.status==404)
-                    console.log(res.status,url)
-                .then.process(); //Termination 
-                    
-                })
-                .catch((error)=>{
-                    console.log("404",url.red, _label.bad.bgRed)
-                })
-            })
-            }
-        })  
-    }
+if(process.argv.length==2){
+    greetingMessage();
 }
-// done
+else{
+    const filePath=path.join(__dirname,process.argv[2])
+    fs.readFile(filePath,'utf-8',(err,data)=>{
+        if(err){
+            console.log("Fail to read file",err)
+        }
+        else{
+            const validUrl=data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi)
+            validUrl.forEach((url)=>{
+            fetch(url,{method:'HEAD',timeout:2000})
+            .then((res)=>{
+                if(res.status==200)
+                console.log(res.status,url.green,_label.good.rainbow)
+                else if(res.status==400||res.status==404)
+                console.log(res.status,url)
+                else console.log(res.status,url)
+                .then.process(); //Termination 
+
+            })
+            .catch((error)=>{
+                console.log("404",url.red, _label.bad.bgRed)
+            })
+        })
+        }
+    })
+}
