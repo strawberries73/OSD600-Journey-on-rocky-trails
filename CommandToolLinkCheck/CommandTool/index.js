8e00839e9f916f50993c1776cd79e2a0cdbaa2e7
Jenn <39388566+strawberries73@users.noreply.github.com>
2020-11-24 11:41:02 -0500

    Updated index.js to made code more modular

--- CommandToolLinkCheck/CommandTool/index.js
+++ CommandToolLinkCheck/CommandTool/index.js
@@ -1,21 +1,21 @@
-const fs=require("fs");
-const fetch=require("node-fetch");
-const path=require("path");
-const packageJson = require("./package.json");
-const colors = require("colors");
+const fs=require('fs')
+const fetch=require('node-fetch')
+const path=require('path')
+const packageJson = require('./package.json');
+const colors = require('colors');
 
 // ExitCode
-process.on("SIGTERM", () => {
+process.on('SIGTERM', () => {
     server.close(() => {
-      console.log("Program is terminated");
-    });
-  });
+      console.log('Program is terminated')
+    })
+  })
 
 //Flag labeling each URL as a good or bad
 const _label = ({
     good: "GOOD",
     bad: "BAD"
-});
+})
 
 //const getTelescope = fetch('http:/') data
 async function getTelescopeData(){
@@ -27,7 +27,7 @@ async function getTelescopeData(){
         
 
         //truncate the data and write to file
-        fs.truncate("telescopeData.txt", 0, function() {
+        fs.truncate('telescopeData.txt', 0, function() {
             for(i = 0; i < data.length; i++) {
                 fetch(`http://localhost:3000${data[i].url}`)
                 .then(res => {
@@ -40,45 +40,51 @@ async function getTelescopeData(){
                                 console.log(err)
                                 .then.process(); //Termination 
                                 } 
-                            });
-                        });
+                            })
+                        })
                     }
-                });
+                })
         }
-    );
+    )
 }
 
 getTelescopeData();
-
 
 if(process.argv.length==2){
     greetingMessage();
     console.log("Hello");
 }
 else{
-    const filePath=path.join(__dirname,process.argv[2]);
-    fs.readFile(filePath,"utf-8",(err,data)=>{
-        if(err){
-            console.log("Fail to read file",err);
-        }
-        else{
-            const validUrl=data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);
-            validUrl.forEach((url)=>{
-            fetch(url,{method:"HEAD",timeout:2000})
-            .then((res)=>{
-                if(res.status==200)
-                console.log(res.status,url.green,_label.good.rainbow);
-                else if(res.status==400||res.status==404)
-                console.log(res.status,url);
-                else console.log(res.status,url)
-                .then.process(); //Termination 
-
-            })
-            .catch((error)=>{
-                console.log("404",url.red, _label.bad.bgRed);
-            });
-        });
-        }
-    });
+    
+    if (process.argv[2] == "v" || process.argv[2] == "version") {
+        console.log(packageJson.name + " Version " + packageJson.version);
+    } else {
+        const filePath = path.join(__dirname, process.argv[2])
+        processLinks(filePath);
+    }
 }
 
+function processLinks(p_filePath) {
+    fs.readFile(p_filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Fail to read file", err)
+        }
+        else {
+            const validUrl = data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi)
+            validUrl.forEach((url) => {
+                fetch(url, { method: 'HEAD', timeout: 2000 })
+                    .then((res) => {
+                        if (res.status == 200)
+                            console.log(res.status, url.green, _label.good.rainbow)
+                        else if (res.status == 400 || res.status == 404)
+                            console.log(res.status, url)
+                                .then.process(); //termination 
+ 
+                    })
+                    .catch((error) => {
+                        console.log("404", url.red, _label.bad.bgRed)
+                    })
+            })
+        }
+    })
+}
