c6424bdd6d7ced65be13be67961f52a8d5f963d6
strawberries73 <39388566+strawberries73@users.noreply.github.com>
2020-11-24 11:25:49 -0500

    Made changes to index

--- CommandToolLinkCheck/CommandTool/index.js
+++ CommandToolLinkCheck/CommandTool/index.js
@@ -50,34 +50,41 @@ async function getTelescopeData(){
 
 getTelescopeData();
 
-
 if(process.argv.length==2){
     greetingMessage();
     console.log("Hello");
 }
 else{
-    const filePath=path.join(__dirname,process.argv[2])
-    fs.readFile(filePath,'utf-8',(err,data)=>{
-        if(err){
-            console.log("Fail to read file",err)
-        }
-        else{
-            const validUrl=data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi)
-            validUrl.forEach((url)=>{
-            fetch(url,{method:'HEAD',timeout:2000})
-            .then((res)=>{
-                if(res.status==200)
-                console.log(res.status,url.green,_label.good.rainbow)
-                else if(res.status==400||res.status==404)
-                console.log(res.status,url)
-                else console.log(res.status,url)
-                .then.process(); //Termination 
+    
+    if (process.argv[2] == "v" || process.argv[2] == "version") {
+        console.log(packageJson.name + " Version " + packageJson.version);
+    } else {
+        const filePath = path.join(__dirname, process.argv[2])
+        processLinks(filePath);
+    }
+}
 
+function processLinks(p_filePath) {
+    fs.readFile(p_filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Fail to read file", err)
+        }
+        else {
+            const validUrl = data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi)
+            validUrl.forEach((url) => {
+                fetch(url, { method: 'HEAD', timeout: 2000 })
+                    .then((res) => {
+                        if (res.status == 200)
+                            console.log(res.status, url.green, _label.good.rainbow)
+                        else if (res.status == 400 || res.status == 404)
+                            console.log(res.status, url)
+                                .then.process(); //termination 
+ 
+                    })
+                    .catch((error) => {
+                        console.log("404", url.red, _label.bad.bgRed)
+                    })
             })
-            .catch((error)=>{
-                console.log("404",url.red, _label.bad.bgRed)
-            })
-        })
         }
     })
 }
