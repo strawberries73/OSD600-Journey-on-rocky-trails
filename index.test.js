d491a29562fdc3c2b12ec5618521c92c25f10222
Jenn <39388566+strawberries73@users.noreply.github.com>
2020-11-29 10:52:18 -0500

    Took out the extra true that is not needed

--- index.test.js
+++ index.test.js
@@ -3,5 +3,5 @@ const { processLinks } = require("./index.js");
 
 test("Pass file with valid links", async () => {
   const result = await processLinks("./validLinks.txt");
-  expect(result).toEqual([true, true]);
+  expect(result).toEqual(true);
 });
